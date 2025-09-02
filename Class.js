76faf7e92a8f11cf5c852578964d4bdc2a117850
aditya102adi <aditya102adi@gmail.com>
2025-09-02 15:05:16 +0530

    inheritance

--- Class.js
+++ Class.js
@@ -3,7 +3,7 @@ class Emoloyee {
     name;
     salary;
 
-    work () {
+    work() {
         console.log(this.name," is working");
     }
 
@@ -16,3 +16,11 @@ const e1 = new Emoloyee();
 e1.id = 101;
 e1.name = "Aditya";
 e1.salary = 45000;
+
+const e2 = new Emoloyee();
+e2.id = 102;
+e2.name = "Ravi";
+e2.salary = 50000;
+
+e1.work();
+e2.work();
---
+++ inheritance.js
@@ -0,0 +1,62 @@
+// Parent class
+class Employee {
+    constructor(name, id, salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    displayDetails() {
+        console.log(`Name: ${this.name}, ID: ${this.id}, Salary: ${this.salary}`);
+    }
+
+    skillsInfo() {
+        console.log(`${this.name}'s Skills: ${this.skills}`);
+    }
+}
+
+// Child class Developer
+class Developer extends Employee {
+    constructor(name, id, salary, skills) {
+        super(name, id, salary); // call parent constructor
+        this.skills = skills;
+    }
+
+    skillsInfo() {
+        console.log(`${this.name}'s Developer Skills: ${this.skills}`);
+    }
+
+    project(projectName) {
+        console.log(`${this.name} is working on project: ${projectName}`);
+    }
+}
+
+// Child class Tester
+class Tester extends Employee {
+    constructor(name, id, salary, tools) {
+        super(name, id, salary);
+        this.tools = tools;
+    }
+
+    skillsInfo() {
+        console.log(`${this.name} tests using tools: ${this.tools}`);
+    }
+
+    project(projectName) {
+        console.log(`${this.name} is testing project: ${projectName}`);
+    }
+}
+
+// Creating Developer object
+const dev = new Developer("Alice", 101, 60000, "JavaScript, React, Node.js");
+dev.displayDetails();
+dev.skillsInfo();
+dev.project("E-Commerce Website");
+
+console.log("-------------------");
+
+// Creating Tester object
+const tester = new Tester("Bob", 102, 50000, "Selenium, Postman");
+tester.displayDetails();
+tester.skillsInfo();
+tester.project("Banking Application");
