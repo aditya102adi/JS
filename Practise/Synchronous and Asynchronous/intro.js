bb4ab1b61d0ec0cc691e0cfea9eac650ecb8c772
aditya102adi <aditya102adi@gmail.com>
2025-09-10 15:11:18 +0530

    Asynchronous

---
+++ Practise/Synchronous and Asynchronous/intro.js
@@ -0,0 +1,34 @@
+//Whenever we use setTimeOut function it becomes Synchronous function
+
+setTimeout(() => {
+    console.log(`Open Book My Show`);
+}, 3000);
+
+setTimeout(() => {
+    console.log(`Search Movie`);
+}, 2000);
+
+setTimeout(() => {
+    console.log(`Select Seats`);
+}, 5000);
+
+setTimeout(() => {
+    console.log(`Ticket Booked`);
+}, 1000);
+
+
+
+
+
+setTimeout(() => {
+    console.log('Step 1');
+    setTimeout(() => {
+        console.log('Step 2');
+        setTimeout(() =>{
+            console.log('Step 3');
+            setTimeout(() => {
+                console.log('Step 4')
+            }, 1000);
+        },5000);
+    },2000);
+},3000);
--- Practise/closure.js
+++ Practise/closure.js
@@ -10,3 +10,20 @@ function outerFunction() {
 const fun = outerFunction();
 fun();
 fun();
+
+
+function outer() {
+    let count = 0; // variable in outer scope
+
+    function inner() {
+        count++; // inner function accessing outer variable
+        console.log(count);
+    }
+
+    return inner; // return the inner function
+}
+
+const counter = outer(); // outer() runs, returns inner()
+counter(); // 1
+counter(); // 2
+counter(); // 3
