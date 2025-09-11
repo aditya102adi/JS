e35802d05ae4858401ce958a2cdef669cf7b944d
aditya102adi <aditya102adi@gmail.com>
2025-09-11 15:20:22 +0530

    Exception

---
+++ Exception Hadling/app.js
@@ -0,0 +1,23 @@
+try {
+    abc();
+} catch (ex) {
+    console.log(ex.messsage);
+} finally {
+    console.log(`Executation completed.`);
+}
+
+//Custom execption 
+function validate(age) {
+    try {
+        if (age < 18) {
+            throw new Error("Under Age");
+        } else {
+            console.log('Age is valid');
+        }
+    } catch (ex) {
+        console.log(ex.messsage);
+    }
+}
+
+validate(22);
+validate(5);
