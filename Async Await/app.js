4e65b7c000951493f37e5c8c3e3e7f083ca412b0
aditya102adi <aditya102adi@gmail.com>
2025-09-12 23:24:43 +0530

    HTML

--- Async Await/app.js
+++ Async Await/app.js
@@ -13,3 +13,69 @@ async function BokkTicket() {
 }
 
 BokkTicket();
+
+
+/*===============================================*/
+//Optimize the code
+
+function wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function BokkTicket() {
+    await wait(2000);
+    console.log('Open BMS');
+
+    await wait(3000);
+    console.log('Select the Movie');
+
+    await wait(5000);
+    console.log('Select Seats');
+
+    await wait(1000);
+    console.log('Ticket Booked');
+}
+
+BokkTicket();
+
+/*=====================================================*/
+
+// Resolve and Reject 
+function wait(ms, shouldFail = false) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (shouldFail) {
+                reject(new Error("BMS Not Found ❌"));
+            } else {
+                resolve("Success ✅");
+            }
+        }, ms);
+    });
+}
+
+async function BokkTicket() {
+    try {
+        await wait(2000); // ✅ success
+        console.log('Open BMS');
+
+        await wait(3000); // ✅ success
+        console.log('Select the Movie');
+
+        await wait(5000); // ✅ success
+        console.log('Select Seats');
+
+        await wait(1000); // ✅ success
+        console.log('Ticket Booked');
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+    finally {
+        console.log('Close Phone');
+    }
+}
+
+BokkTicket();
+
+
+//Coffe Making
